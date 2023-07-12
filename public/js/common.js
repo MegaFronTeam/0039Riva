c353a3f421e989e18f28273c733f671877c021d4
ilyaBass <vishnyak_ilya@mail.ru>
2023-07-12 20:27:19 +0300

    d3/layout

--- public/js/common.js
+++ public/js/common.js
@@ -435,6 +435,30 @@ function eventHandler() {
 
 	});
 
+	const headerBlockSwiper = new Swiper('.headerBlock__slider--js', {
+		slidesPerView: 1,
+		// ...defaultSl,
+		// slidesPerView: 'auto',
+		// freeMode: true,
+		// loopFillGroupWithBlank: true,
+		// touchRatio: 0.2,
+		// slideToClickedSlide: true,
+		// freeModeMomentum: true,
+		// loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: ' .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			// renderBullet: function (index, className) {
+			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+			// }
+		},
+	});
+
 	// modal window
 
 	
